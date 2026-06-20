fa598831f83605f6ae6c07c91b8b9aa36dacb890
Aqua <ollie@ollierobinson.co.uk>
2026-06-20 19:22:44 +0100

    add cpptrace

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include "Game.h"
 
+#include <cpptrace/cpptrace.hpp>
+#include <cpptrace/from_current.hpp>
+
+#include <stdexcept>
+#include <format>
+
 int main()
 {
-    birdle::Game game;
-    game.run();
+    CPPTRACE_TRY
+    {
+        birdle::Game game;
+        game.run();
+    }
+    CPPTRACE_CATCH (const std::exception& e)
+    {
+        const auto& stacktrace = cpptrace::from_current_exception();
+        
+        SDL_ShowSimpleMessageBox(
+            SDL_MESSAGEBOX_ERROR, "birdle",
+            std::format("Oops! birdle crashed.\n{}\n{}", e.what(), stacktrace.to_string()).c_str(), nullptr);
+        
+        stacktrace.print();
+        cpptrace::rethrow();
+    }
     return 0;
 }
